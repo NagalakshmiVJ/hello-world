55e75b97bd4c9b59b4778722e4c436ea630ea831
NagalakshmiVJ <37115332+NagalakshmiVJ@users.noreply.github.com>
2018-03-12 19:40:20 +0530

    Create numbertype.java

---
+++ numbertype.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class numbertype
+{
+static int n;
+public static void main(String Arg[])
+{
+System.out.println("Enter a number: ");
+Scanner s = new Scanner(System.in);
+n=s.nextInt();
+if(n>0)
+System.out.println("Positive");
+else if(n<0)
+System.out.println("Negative");
+else
+System.out.println("Zero");
+}
+}

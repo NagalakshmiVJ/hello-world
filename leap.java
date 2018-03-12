12bc3dd47e40b21abf7f849d1c7e4f4f93c895db
NagalakshmiVJ <37115332+NagalakshmiVJ@users.noreply.github.com>
2018-03-12 20:07:31 +0530

    Create leap.java

---
+++ leap.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class leap
+{
+public static void main(String arg[])
+{
+int n;
+Scanner s=new Scanner(System.in);
+n=s.nextInt();
+if(n%4==0)
+System.out.println("yes");
+else
+System.out.println("no");
+}
+}

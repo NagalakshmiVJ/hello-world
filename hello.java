16f8820f5244732b9e518cc69029c3ec7f86ce05
NagalakshmiVJ <37115332+NagalakshmiVJ@users.noreply.github.com>
2018-03-12 20:11:00 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class hello
+{
+public static void main(String arg[])
+{
+int n;
+Scanner s=new Scanner(System.in);
+n=s.nextInt();
+for(int i=0;i<n;i++)
+System.out.println("Hello");
+}
+}

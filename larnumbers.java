751bcb889b42d7ae6f34363a4b9efc18bba12871
NagalakshmiVJ <37115332+NagalakshmiVJ@users.noreply.github.com>
2018-03-14 21:26:23 +0530

    Create larnumbers.java

---
+++ larnumbers.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class larnumbers
+{
+public static void main(String arg[])
+{
+int num1,num2,num3;
+Scanner s= new Scanner(System.in);
+num1=s.nextInt();
+num2=s.nextInt();
+num3=s.nextInt();
+if((num1>num2)&&(num1>num3))
+System.out.println(num1);
+else if((num2>num1)&&(num2>num3))
+System.out.println(num2);
+else 
+System.out.println(num3);
+}
+}

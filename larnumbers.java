9deba137afb9971ec39505cddb91a0246817e795
NagalakshmiVJ <37115332+NagalakshmiVJ@users.noreply.github.com>
2018-03-14 21:28:48 +0530

    Update larnumbers.java

--- larnumbers.java
+++ larnumbers.java
@@ -10,7 +10,7 @@ num2=s.nextInt();
 num3=s.nextInt();
 if((num1>num2)&&(num1>num3))
 System.out.println(num1);
-else if((num2>num1)&&(num2>num3))
+else if(num2>num3)
 System.out.println(num2);
 else 
 System.out.println(num3);
